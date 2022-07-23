2a279fafd5b8112561e271c0e535a69545fda052
zoufuqun <850373266@qq.com>
2022-07-24 07:08:50 +0800

    初始化项目

---
+++ men-spring/men-spring-nacos-service/src/main/java/com/men/UserApplication.java
@@ -0,0 +1,16 @@
+package com.men;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zoufuqun
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
---
+++ men-spring/men-spring-nacos-service/src/main/java/com/men/config/MainConfig.java
@@ -0,0 +1,11 @@
+package com.men.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zoufuqun
+ */
+@Configuration
+public class MainConfig {
+
+}
